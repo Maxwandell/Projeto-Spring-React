7a487ed3d8c8f889fa0ef8e5108393d50fc405e5
Maxwandell <Maxwandell.silva@dce.ufpb.br>
2021-05-03 21:04:04 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="Maxwandell$$MariaLoraana" target="_blank" rel="noreferrer">Maxwandell</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+       se ela aceitar eu dibulgo o insta dela: <a href="https://instagram.com/maria.lorrana" target="_blank" rel="noreferrer">@maria.lorrana</a></small></p>
+  </div>
+</footer>
+    );
+  }
+  
+  export default Footer;
