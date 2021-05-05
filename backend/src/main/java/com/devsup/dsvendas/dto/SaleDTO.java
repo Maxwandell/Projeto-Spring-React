2241f24162a83c40124ec0b1866e2c9c8e05c5ce
Maxwandell <Maxwandell.silva@dce.ufpb.br>
2021-05-05 19:01:34 -0300

    pagination

--- backend/src/main/java/com/devsup/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsup/dsvendas/dto/SaleDTO.java
@@ -1,5 +1,100 @@
 package com.devsup.dsvendas.dto;
 
+import java.time.LocalDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.devsup.dsvendas.entitities.Sale;
+import com.devsup.dsvendas.entitities.Seller;
+
 public class SaleDTO {
+	
+	
+	private Long id;
+	
+	private Integer visited;
+	
+	private Integer deals;
+	
+	private Double amount;
+	
+	private LocalDate date;
+	
+	
+	private SellerDTO seller;
+
+	public SaleDTO() {
+		
+	}
+
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+
+	public SaleDTO(Sale entity) {
+		this.id =entity.getId();
+		this.visited =entity.getVisited();
+		this.deals = entity.getDeals();
+		this.amount = entity.getAmount();
+		this.date =entity.getDate();
+		this.seller = new SellerDTO(entity.getSeller());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public SellerDTO getSeller() {
+		return seller;
+	}
+
+	public void setSeller(SellerDTO seller) {
+		this.seller = seller;
+	}
 
 }
--- backend/src/main/java/com/devsup/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsup/dsvendas/service/SaleService.java
@@ -1,10 +1,14 @@
 package com.devsup.dsvendas.service;
 
-import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsup.dsvendas.dto.SaleDTO;
 import com.devsup.dsvendas.entitities.Sale;
 import com.devsup.dsvendas.repositories.SaleRepository;
 
@@ -14,8 +18,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepo;
 	
-	public List<Sale> findAll(){
-		return saleRepo.findAll();
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		 Page<Sale> result = saleRepo.findAll(pageable);
+		 return result.map(x -> new SaleDTO(x));
 	}
 
 }
+
